1134ce4fb2da9c4944cd80a9dd91d55a9e9092d7
Wulung Triyanto <wulung.triyanto@outlook.com>
2023-11-15 08:59:15 +0700

    initial commit via upload

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	swagger "github.com/arsmn/fiber-swagger/v2"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/recover"
+)
+
+// @title Fiber Example API
+// @version 1.0
+// @description This is a sample swagger for Fiber
+// @termsOfService http://swagger.io/terms/
+// @contact.name API Support
+// @contact.email fiber@swagger.io
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+// @host localhost:8080
+// @basePath /
+// @schemas http
+func main() {
+	// Fiber instance
+	app := fiber.New()
+
+	// Middleware
+	app.Use(recover.New())
+	app.Use(cors.New())
+
+	// Default swagger handler
+	app.Get("/swagger/*", swagger.HandlerDefault)
+
+	// Start server
+	app.Listen(":8080")
+}
+
+// HealthCheck godoc
+// @Summary Show the status of server.
+// @Description get the status of server.
+// @Tags root
+// @Accept */*
+// @Produce json
+// @Success 200 {object} map[string]interface{}
+// @Router / [get]
+func HealthCheck(c *fiber.Ctx) error {
+	res := map[string]interface{}{
+		"data": "Server is up and running",
+	}
+
+	if err := c.JSON(res); err != nil {
+		return err
+	}
+
+	return nil
+}
